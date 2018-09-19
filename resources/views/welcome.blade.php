c09418c53195126a1170a88aac35e04ab9b66d51
Marleen <aan_marleen@hotmail.com>
2018-09-19 21:56:11 +0200

    Inloggen en css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,64 +8,12 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="{{ url('css/DashboardStyle.css') }}">  <!-- url kan ook asset -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-<!--
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-        -->
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -73,6 +21,8 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
+                        <a>{{ Auth::user()->name }}</a>
+
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -81,6 +31,9 @@
             @endif
 
             <div class="content">
+                <div class="title m-b-md">
+                    Memory Dashboard
+                </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
@@ -89,10 +42,6 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-
-                <div class="title m-b-md">
-                    Memory
                 </div>
 
             </div>
